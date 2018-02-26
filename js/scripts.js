08e4d24fb22ff2f78a0e4d44357b8be22342dddc
Maciej Kubas <maciekkubas@gmail.com>
2018-02-26 20:17:13 +0100

    added js code

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//scripts.js
+
+var a = prompt('Base of triangle a');
+var h = prompt('Height of triangle h');
+var triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
